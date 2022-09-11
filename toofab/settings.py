9e106fec950abdbb36dc12655590a0aa95133fba
dimafoont <foontee4ek@gmail.com>
2022-09-11 18:19:49 +0300

    edit setting debug true db

--- toofab/settings.py
+++ toofab/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'toofab.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'toofab',
+        'USER': 'toofabuser',
+        'PASSWORD': 'toofab998156',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'toofab',
-            'USER': 'toofabuser',
-            'PASSWORD': 'toofab998156',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
-    }
+}
 
 
 # Password validation
